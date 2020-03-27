926647ccfcbdaf1ac32c9d58b4268f3d56e6d069
Ángel Sánchez Romero <sanchezangel.16@outlook.com>
2020-03-27 09:28:40 -0600

    player insertion

--- app.py
+++ app.py
@@ -16,18 +16,13 @@ padding = 10
 value = 0
 board = [[0] * 7 for i in range(8)]
 win = pygame.display.set_mode((s_width, s_height))
-
-
-def set_board():
-    for i in range(8):
-        for j in range(7):
-            board[i][j] = random.randrange(0,3)
+player_pos = 1
 
 def draw_board(win):
     for i in range(8):
         for j in range(7):
-            x_pos = (i*circle_radius+circle_radius + (i*circle_radius))
-            y_pos = (j*circle_radius+circle_radius + (j*circle_radius) + 65)
+            x_pos = (i*circle_radius+circle_radius + (i*circle_radius) + 10)
+            y_pos = (j*circle_radius+circle_radius + (j*circle_radius) + 75)
             if board[i][j] == 0:
                 pygame.draw.circle(win, BLACK, (x_pos, y_pos), circle_radius)
             elif board[i][j] == 1:
@@ -35,18 +30,40 @@ def draw_board(win):
             else:
                 pygame.draw.circle(win, RED, (x_pos, y_pos), circle_radius)
                 
-            
+def insert_on_board(pos_x, turn):
+    insertion = 0
+    for i in range(7):
+        if board[pos_x][i] == 0:
+            insertion = i
+    board[pos_x][insertion] = turn
 
 def main():
     run = True
-    set_board()
+    player_pos = 0
+    turn = 1
     while run:
-        win.fill(BLUE)
+        win.fill(BLUE, (0,70,s_width,s_height))
+        win.fill(BLACK, (0,0, s_width, 70))
         draw_board(win)
+        if turn == 1:
+            pygame.draw.circle(win, YELLOW, (40 + (player_pos*60),35), circle_radius)
+        else:
+            pygame.draw.circle(win, RED, (40 + (player_pos*60),35), circle_radius)
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT and player_pos < 7:
+                    player_pos += 1
+                if event.key == pygame.K_LEFT and player_pos > 0:
+                    player_pos -= 1
+                if event.key == pygame.K_RETURN:
+                    insert_on_board(player_pos, turn)
+                    if turn == 1:
+                        turn = 2
+                    else:
+                        turn = 1
     pygame.display.quit()
 
 main()
